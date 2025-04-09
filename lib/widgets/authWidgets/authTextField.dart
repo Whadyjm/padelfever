9c8420b6b7a30daee3c1b0faabe7485190ad9e9c
Whady <martinez.whady@gmail.com>
2025-04-08 21:06:04 -0400

    inicio de autenticacion

---
+++ lib/widgets/authWidgets/authTextField.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:padelpoint/theme/text.dart';
+
+class AuthTextField extends StatelessWidget {
+  const AuthTextField({
+    super.key,
+    required this.nombre,
+    required this.hintText,
+  });
+
+  final TextEditingController nombre;
+  final String hintText;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      width: 230,
+      child: TextFormField(
+        maxLength: 25,
+        style: AppText.smallTextStyle(Colors.grey.shade700),
+        controller: nombre,
+        cursorColor: Colors.blue,
+        decoration: InputDecoration(
+          counterText: "",
+          hintText: hintText,
+          hintStyle: AppText.verySmallTextStyle(Colors.grey.shade500),
+          filled: true,
+          fillColor: Colors.white,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(20)),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(width: 1.5, color: Colors.grey.shade400),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue),
+          ),
+        ),
+      ),
+    );
+  }
+}
